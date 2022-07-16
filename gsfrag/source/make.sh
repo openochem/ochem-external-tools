706f75be3fbe1ba12dfc8f8eca09685b1dd869b4
bigchem <47360534+bigchem@users.noreply.github.com>
2022-07-16 13:56:18 +0200

    Fix in compilation script of GSFRag

--- gsfrag/source/make.sh
+++ gsfrag/source/make.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
-g++ -I/usr/include/x86_64-linux-gnu/c++/4.8 -static -O3 global.cpp graph.cpp strsrcSDF.cpp struct.cpp gsfrag.cpp -o gsfrag
-g++  -I/usr/include/x86_64-linux-gnu/c++/4.8 -static -O3 global.cpp graph.cpp strsrcSDF.cpp struct.cpp gsfragl.cpp -o gsfragl
+g++ -static -O3 global.cpp graph.cpp strsrcSDF.cpp struct.cpp gsfrag.cpp -o gsfrag
+g++ -static -O3 global.cpp graph.cpp strsrcSDF.cpp struct.cpp gsfragl.cpp -o gsfragl
