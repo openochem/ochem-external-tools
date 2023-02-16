a67cf23ffe5bd7d71e25b15017b6d803ab851fb5
bigchem <47360534+bigchem@users.noreply.github.com>
2023-02-16 22:51:49 +0100

    Support to start ochem instances on ports determined by ochemenvXXXX

--- ochem/bin/startochem.sh
+++ ochem/bin/startochem.sh
@@ -2,20 +2,35 @@
 
 for var in "$@"
 do
-    export "$var"
+    if [[ $var =~ ^ochemenv(.+) ]]; then
+        port=`echo "$var" | sed 's/ochemenv//'`
+    else
+        export "$var"
+    fi
 done
 
 cd /etc/ochem ; unzip -o /home/ochem.zip
 cp /ochem/cfg/* .
 
 unzip -o /ochem/ochem.release -d /ochem/tmp/cs_release/
-#mv /ochem/tmp/cs_release/ochem.war /etc/ochem/ochem-tomcat/webapps/ROOT.war
-#mv /ochem/tmp/cs_release/metaserver.war /etc/ochem/metaserver-tomcat/webapps/metaserver.war
+
+if [ -z "$port" ]; then 
+		echo "using default port 8080"; 
+	else 
+		echo "using port '$port'";
+		sed -i -r "s|8080|$port|g" /etc/ochem/ochem-tomcat/conf/server.xml
+		summa=`expr 9009 + $port - 8080`
+		echo "using port '$summa'";
+		sed -i -r "s|8009|$summa|g" /etc/ochem/ochem-tomcat/conf/server.xml
+		summa=`expr 9006 + $port - 8080`
+		sed -i -r "s|8005|$summa|g" /etc/ochem/ochem-tomcat/conf/server.xml
+fi
 
 #OCHEM
 mkdir -p /etc/ochem/ochem-tomcat/webapps/ROOT
 cd /etc/ochem/ochem-tomcat/webapps/ROOT
-jar -xvf /ochem/tmp/cs_release/ochem.war
+jar -xvf /ochem/tmp/cs_release/ochem.war >/dev/null 2>/dev/null
+
 cd js
 rm -rf lib; mkdir lib
 cp -r /etc/source/javascripts/* lib
@@ -23,14 +38,15 @@ rm -rf lib/jquery-1.4.1.min.js
 cp /etc/source/cdk/* /etc/ochem/ochem-tomcat/webapps/ROOT/WEB-INF/lib/ # can be used to update CDK
 cp /etc/source/chem/* /etc/ochem/ochem-tomcat/webapps/ROOT/WEB-INF/lib/ # any additional code to be copied can be added here
 
+if [ -z "$port" ]; then 
+	#METASERVER
+	mkdir -p /etc/ochem/metaserver-tomcat/webapps/metaserver
+	cd /etc/ochem/metaserver-tomcat/webapps/metaserver
+	jar -xvf /ochem/tmp/cs_release/metaserver.war >/dev/null 2>/dev/null
+	cd js
+	cp /etc/source/javascripts/jquery.flot.js .
+	cp /etc/source/javascripts/jquery-1.4.1.min.js .
+	sh /etc/ochem/ochem/bin/metaserver-tomcat start
+fi
 
-#METASERVER
-mkdir -p /etc/ochem/metaserver-tomcat/webapps/metaserver
-cd /etc/ochem/metaserver-tomcat/webapps/metaserver
-jar -xvf /ochem/tmp/cs_release/metaserver.war
-cd js
-cp /etc/source/javascripts/jquery.flot.js .
-cp /etc/source/javascripts/jquery-1.4.1.min.js .
-
-sh /etc/ochem/ochem/bin/metaserver-tomcat start
 sh /etc/ochem/ochem/bin/ochem-tomcat start
