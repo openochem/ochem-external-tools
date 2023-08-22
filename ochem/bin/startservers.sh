61c4b4b2e1b817b2f1808d34df18bd3130b3a8a3
bigchem <47360534+bigchem@users.noreply.github.com>
2023-08-22 18:27:55 +0200

    decreasing log

--- ochem/bin/startservers.sh
+++ ochem/bin/startservers.sh
@@ -6,7 +6,8 @@ mkdir /tmp/.screen && chmod 700 /tmp/.screen
 rm -rf /etc/cs_servers/*
 mkdir /etc/cs_servers/server1
 cd /etc/cs_servers/server1
-unzip /etc/ochem/release.zip
+echo "unzip /etc/ochem/release.zip"
+unzip /etc/ochem/release.zip >/dev/null
 cd /etc/cs_servers/
 ln -s server1/tools/utils/* .
 perl monitor.pl > monitor.log  &
